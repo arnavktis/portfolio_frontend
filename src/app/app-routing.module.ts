9e3a6651ac8904f5bfdc029e1416a83851eb6039
Arnav khanna <arnavktis@gmail.com>
2024-01-07 13:10:08 +0530

    header component
    header-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: 'home', component: HomePageComponent},
+    {path: '', component: HomePageComponent}
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
