435380b1f86cbca7b21cb5823d187226bc2e90e2
ansoni8 <148438972+ansoni8@users.noreply.github.com>
2024-01-07 16:11:00 +0530

    landing home page
    home-page landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,8 @@ import { HomePageComponent } from './home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
 
+export class HomePageComponent implements OnInit {
+  ngOnInit(): void {
+    const options = {
+      strings: ['Frontend Developer.', 'Backend Developer.'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+  };
+  const typed = new Typed('.typed-element', options);
+  }
 }
