05f79f2fe74062a1b7f4fd0eab7c21f745a16e36
Arnav khanna <arnavktis@gmail.com>
2024-03-03 13:04:56 +0530

    .

---
+++ _site/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot([
+    {path: 'home', component: HomePageComponent},
+    {path: '', component: HomePageComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'skills', component: SkillsComponent},
+    {path: 'certificate', component: CertificateComponent},
+    {path: 'contacts', component: ContactsComponent}
+  ]),
+  RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ _site/src/app/certificate/certificate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-certificate',
+  templateUrl: './certificate.component.html',
+  styleUrls: ['./certificate.component.scss']
+})
+export class CertificateComponent {
+
+}
